7077996e511080ea1f574478c206f4ced36accde
John McAvey <john@users-Mac-Pro.local>
2014-11-04 23:20:30 -0600

    Initial Commit

---
+++ Hendrix Course Scheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hendrix Course Scheduler
+//
+//  Created by John McAvey on 11/4/14.
+//  Copyright (c) 2014 Survey Dream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hendrix Course SchedulerTests/Hendrix_Course_SchedulerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hendrix_Course_SchedulerTests.swift
+//  Hendrix Course SchedulerTests
+//
+//  Created by John McAvey on 11/4/14.
+//  Copyright (c) 2014 Survey Dream. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Hendrix_Course_SchedulerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
